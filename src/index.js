fa3afa4ebf7f9ca800c03d5bdacb1949a09ef679
Teresa Swatloski <t@santacruzwood.com>
2019-08-28 17:53:45 -0700

    enable hot modules

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import App from 'app/App'
 import registerServiceWorker from 'app/registerServiceWorker'
 import { init as rxInit } from 'state/rxInternals'
 
+const rootEl = document.getElementById('root')
+
 ReactDOM.render(
-        <App store={rxInit()} />,
-    document.getElementById('root')
-);
+    <App store={rxInit()} />,
+    rootEl
+)
+
+if (module.hot) {
+    module.hot.accept('./app/App', () => {
+        const NextApp = require('./app/App').default
+        ReactDOM.render(
+            <NextApp />,
+            rootEl
+        )
+    })
+}
 registerServiceWorker();
