823f53670d46b7084ee02c13d68af98f8cb7731c
Teresa Swatloski <t@santacruzwood.com>
2019-10-16 11:57:53 -0700

    fix test data ref

--- src/gene/page.js
+++ src/gene/page.js
@@ -12,7 +12,8 @@ import { summarizeCats, clearCats, catAttrs, gatherUniqueCats }
     from 'color/colorCat'
 import Presentation from 'gene/pagePres'
 import { tableNewData } from 'gene/table'
-import { USE_TEST_DATA } from 'cellTypeSheet/sheetList'
+
+const USE_TEST_DATA = false
 
 let data // the store for data outside of redux state
 
